5b4eb377f7d3c5eab020a480c27b7c4d61dec09c
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-11-19 13:21:32 -0300

    Atualizar o info.js

--- info.js
+++ info.js
@@ -2,12 +2,6 @@ let janela = window.location.href
 
 function showAlert() {
     
-    if(janela.includes("atividades")) {
-  alert("Aqui é onde você verá o que os professores estão Mostrando, postagens que mantém o Aluno atualizado sobre oque está acontecendo, e apenas professores podem publicar.");
-    }
-    if (janela.includes("jogos")) {
-  alert("Aqui você poderá jogar jogos dinâmicos relacionados as disciplinas.");
-}
-    
+    alert("O infoLocal da prioridades a eventos e notícias locais.")
     
 }
